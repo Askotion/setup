5602001096e3ed0f3d0ac29c4512d89d64c4540a
Askotion <askotion@gmail.com>
2016-06-20 23:58:04 +0200

    Completely overwrite hostname script. Hoping for a fix.

--- hostname.sh
+++ hostname.sh
@@ -1,82 +1,59 @@
 #!/bin/bash
 
-echo "1)" >> log.txt
-# Test if the user puts a Argument
-if [[ $# -eq 0 ]] ; then
-    echo 'You have to define in a Hostname!'
-    echo 'Usage: ./hostname.sh <hostname> '
-    exit 0
-fi
+echo "Stage 1:"  >> log.txt
+
+apt-get update >> log.txt
+sleep 1
+apt-get -y upgrade >> log.txt
+sleep 1
+apt-get -y dist-upgrade >> log.txt
+sleep 1
+apt-get -y autoremove >> log.txt
+sleep 1
+apt-get -y install screen
+sleep 1
+
+echo "Stage 2:"  >> log.txt
+
+wget -q master.askoproducts.com/dl/setup/kvmsetup.sh
+sleep 1
+wget -q master.askoproducts.com/dl/setup/starter.sh
+sleep 1
+wget -q master.askoproducts.com/dl/setup/getid.sh
+sleep 1
+wget -q master.askoproducts.com/dl/setup/setupvm.sh
+sleep 1
+
+echo "Stage 3:"  >> log.txt
+
+mv kvmsetup.sh /home/kvmsetup.sh
+sleep 1
+mv starter.sh /home/starter.sh
+sleep 1
+mv getid.sh /root/getid.sh
+sleep 1
+mv setupvm.sh /home/setupvm.sh
+sleep 1
+
+echo "Stage 4:"  >> log.txt
+
+chmod +x /home/kvmsetup.sh
+sleep 1
+chmod +x /home/starter.sh
+sleep 1
+chmod +x /root/getid.sh
+sleep 1
+
+echo "Stage 5:"  >> log.txt
 
 echo '@reboot root /home/starter.sh' >> /etc/crontab
-echo "2)" >> log.txt
 
-# Updating and Upgrading Server
-apt-get update >> log.txt
-echo "3)" >> log.txt
-apt-get -y upgrade >> log.txt
-echo "4)" >> log.txt
-apt-get -y dist-upgrade >> log.txt
-echo "5)" >> log.txt
-echo "6)" >> log.txt
+hostn=$(cat /etc/hostname)
+sed -i "s/$hostn/$1/g" /etc/hosts
+sed -i "s/$hostn/$1/g" /etc/hostname
 
-function wget () {
-echo "7)" >> log.txt &&
-wget -q master.askoproducts.com/dl/setup/kvmsetup.sh &&
-wget -q master.askoproducts.com/dl/setup/starter.sh &&
-wget -q master.askoproducts.com/dl/setup/getid.sh &&
-wget -q master.askoproducts.com/dl/setup/setupvm.sh
-}
+echo "Stage 6:"  >> log.txt
+sleep 5
 
-function mv () {
-echo "8)" >> log.txt &&
-mv kvmsetup.sh /home/kvmsetup.sh &&
-mv starter.sh /home/starter.sh &&
-mv getid.sh /root/getid.sh &&
-mv setupvm.sh /home/setupvm.sh
-}
-
-function chmod () {
-echo "9)" >> log.txt &&
-chmod +x /home/kvmsetup.sh &&
-chmod +x /home/starter.sh &&
-chmod +x /root/getid.sh
-}
-
-
-# Saving Argument 1 in Textfile
-echo $1 >> var.txt
-
-# Grepping Argument out of the File into a variable
-ip=$(cat var.txt)
-
-
-# Replacing Hostname with Argument
-function hostname () {
-sudo apt-get --assume-yes install screen >> log.txt
-echo "10)" >> log.txt &&
-hostn=$(cat /etc/hostname) &&
-sed -i "s/$hostn/$ip/g" /etc/hosts &&
-sed -i "s/$hostn/$ip/g" /etc/hostname
-}
-
-
-# Removing Text File
-rm var.txt
-
-
-# Executing Function
-wget
-
-# Executing Function
-mv
-
-# Executing Function
-chmod
-
-# Executing Function
-hostname
-
-# Rebooting
 reboot
 
