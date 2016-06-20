7596b0485454afaf4f725145c626e87a53b327dd
Askotion <askotion@gmail.com>
2016-06-20 03:40:21 +0200

    Rewrite most of the script due to "Segmentation fault" error

--- hostname.sh
+++ hostname.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+echo "1)" >> log.txt
 # Test if the user puts a Argument
 if [[ $# -eq 0 ]] ; then
     echo 'You have to define in a Hostname!'
@@ -9,31 +9,41 @@ if [[ $# -eq 0 ]] ; then
 fi
 
 echo '@reboot root /home/starter.sh' >> /etc/crontab
-
+echo "2)" >> log.txt
 
 # Updating and Upgrading Server
 apt-get update >> log.txt
+echo "3)" >> log.txt
 apt-get -y upgrade >> log.txt
-apt-get -y install git screen >> log.txt
+echo "4)" >> log.txt
+apt-get -y dist-upgrade >> log.txt
+echo "5)" >> log.txt
+apt-get -y install screen >> log.txt
+echo "6)" >> log.txt
 
-# Git clone directory
-git clone https://github.com/Askotion/setup.git /root/git/
-
-sleep 15
-
-function cp () {
-cp /root/git/kvmsetup.sh /home/kvmsetup.sh &&
-cp /root/git/starter.sh /home/starter.sh &&
-cp /root/git/getid.sh /root/getid.sh &&
-cp /root/git/setupvm.sh /home/setupvm.sh
+function wget () {
+echo "7)" >> log.txt &&
+wget -q master.askoproducts.com/dl/setup/kvmsetup.sh &&
+wget -q master.askoproducts.com/dl/setup/starter.sh &&
+wget -q master.askoproducts.com/dl/setup/getid.sh &&
+wget -q master.askoproducts.com/dl/setup/setupvm.sh
 }
 
+function mv () {
+echo "8)" >> log.txt &&
+mv kvmsetup.sh /home/kvmsetup.sh &&
+mv starter.sh /home/starter.sh &&
+mv getid.sh /root/getid.sh &&
+mv setupvm.sh /home/setupvm.sh
+}
 
 function chmod () {
-chmod +x /home/kvmsetup.sh
-chmod +x /home/starter.sh
+echo "9)" >> log.txt &&
+chmod +x /home/kvmsetup.sh &&
+chmod +x /home/starter.sh &&
 chmod +x /root/getid.sh
 }
+
 
 # Saving Argument 1 in Textfile
 echo $1 >> var.txt
@@ -44,22 +54,27 @@ ip=$(cat var.txt)
 
 # Replacing Hostname with Argument
 function hostname () {
+echo "10)" >> log.txt &&
 hostn=$(cat /etc/hostname) &&
 sed -i "s/$hostn/$ip/g" /etc/hosts &&
 sed -i "s/$hostn/$ip/g" /etc/hostname
 }
 
-# Executing Function
-hostname
 
 # Removing Text File
 rm var.txt
 
 # Executing Function
-cp
+wget
+
+# Executing Function
+mv
 
 # Executing Function
 chmod
+
+# Executing Function
+hostname
 
 # Rebooting
 reboot
