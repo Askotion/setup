eb101586467b3fabf2f5817dd3cea97c2d7ee572
askotion <askotion@gmail.com>
2016-08-10 08:14:03 -0400

    Comment a few things out and change file ending from .qcow2 to .img

--- kvmsetup.sh
+++ kvmsetup.sh
@@ -14,8 +14,8 @@ sed -i '$ d' /etc/crontab
 
 # - Variables - #
 
-sshkeyfirma=\
-'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDcW1wjKTw7auJBgiv5LEmC1wOxJ+JdWVa2lV/7py+mI3MthyAfvYrFU2F1cZ3fnSaCKa+gIU33PWxLztEACBOfCWMtUjRy5GGMnfR2Uwdf1SZoGMv39GMYvrHvCU5rMd9JEvaf5H9Erk7jceZpYYTMeY3FrXO0u6eC5Pmwe+2g1Cbzzz7a9AGdIYnN2L1M+ys1yyn2CEbWQNbv5h2a7RuyVwTcMsalS2Gt/+K1JGzKS8BVZRchayhINaCNQsTSsw5cNTdG5fSuVjFkH8iKhKs7WbE9jFlTiWM1gJbNn/jUx0qoEjkHaJPby2YffSi35nPfcYYaiXtd7THPv3IJHJW/ kprobst@kprobst'
+#sshkeyfirma=\
+#'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDcW1wjKTw7auJBgiv5LEmC1wOxJ+JdWVa2lV/7py+mI3MthyAfvYrFU2F1cZ3fnSaCKa+gIU33PWxLztEACBOfCWMtUjRy5GGMnfR2Uwdf1SZoGMv39GMYvrHvCU5rMd9JEvaf5H9Erk7jceZpYYTMeY3FrXO0u6eC5Pmwe+2g1Cbzzz7a9AGdIYnN2L1M+ys1yyn2CEbWQNbv5h2a7RuyVwTcMsalS2Gt/+K1JGzKS8BVZRchayhINaCNQsTSsw5cNTdG5fSuVjFkH8iKhKs7WbE9jFlTiWM1gJbNn/jUx0qoEjkHaJPby2YffSi35nPfcYYaiXtd7THPv3IJHJW/ kprobst@kprobst'
 
 sshkey=\
 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC1dPKulXFoApU0HVW08evHteJo4PDMJDmv5md3CqGA09Uoje3LjitHZwlYz08o1IuJ04ntt9uH38YRco38oGB+rBqs+EDVdeAh5f4foIL3c66FYz6fknD2JPeSVUNswUzTB2uVXSUrn/EZc2keGxUWxltz18LdJHQnKKd+Rnw440/LMuo9rA250kI4jjZWhH/kS6gLLgtGqxFOCIFa6hnQK30NJu0xlTlou7sg/VGObEGj7hyYJ0TnbfiswZEdqBBD4w8yaQtZe+nREVn13uS2UQq9HTQ3GFL200Yk9jDFcjIFMkV8SNpma3RLsRvSnykVsiY9+EbjXrjOfiFxaQEbdRtaqmJf/EHuE1XDRff7IWJX41RaqHRrfp7tYYzrPnnnyOM4cBU4WearHHKcLq0zk03MtrfMZaHbyRsL5F6RaZfiVw5fsf3evr5ygWP9iyYXJ1+WVsViZT23ldlopVYnX8o00YvRcNhbMGdVDS5wW0uJQfOL4Gnf+mtvPhOr+CJ619yYIuqS5bK/jP2YvFE2hS02J3NVygLZnD1Cl7sNcJklkqpRDqkGl6sl31u9Gq/5C0OjCXEDoBnE1Ocu1o10lDMhzbFwWDSdOPgDID4rFDxZCIR3RWlKU9Lg4YFIsG0ZUAh8bUjW44E/jjdIchgs/2PVLDl/I6nWE6BQWmP5/w== Kay@kay-macbook-pro15.fritz.box'
@@ -23,19 +23,15 @@ sshkey=\
 sshkeyclashauto=\
 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCkUguSEdJnWtPdQto2qRT6MgAg8erVlpTqKJGWzxztNFIWaVJAKggqtxIk980z6J2T6NlNU+UY+JcAqzZQaevAgoIRJo3nL/kJAAZgRj7UGiKNkSwYIVEhZxeVd1GIaSfJmBk85eu/lqQF4z8dMjvWGH1xgEdVv/LFdTh9Ta/iX8zUsKGUHhLaZg+eXCULZzAjLkAyoBzfx1kyUF+splXf0kCmP1srA+ryovPyB8mfZJy1OJe15RceT1ZszeA8qbM0HZd/wYxChA2q7/3i/rSY5hYW7auRQMzctrL+2z4OiEn4n1aArMvemf84sJ7wqiRnEKmprZWrgxECW+bHTGmh clashauto@Master'
 
-virtimport=\
-'virt-install -n ClashAuto01 -r 4096 --os-type=windows --vcpus=2 --os-variant=win2k8 --disk /home/CAServer01.qcow2,device=disk,bus=ide,format=qcow2 --network bridge=virbr0,model=virtio --graphics vnc,password=clashauto999,port=5911,keymap=de,listen=0.0.0.0 --noautoconsole --import'
-
-
-# - Variables - #
-
+#virtimport=\
+#'virt-install -n ClashAuto01 -r 4096 --os-type=windows --vcpus=2 --os-variant=win2k8 --disk /home/CAServer01.qcow2,device=disk,bus=ide,format=qcow2 --network bridge=virbr0,model=virtio --graphics vnc,password=clashauto999,port=5911,keymap=de,listen=0.0.0.0 --noautoconsole --import'
 
 
 
 # - Functions - #
 
 function update() {
-echo "$sshkeyfirma" >>/root/.ssh/authorized_keys &&
+echo "$sshkeyclashauto" >>/root/.ssh/authorized_keys &&
 echo "$sshkey" >>/root/.ssh/authorized_keys
 }
 
@@ -48,9 +44,9 @@ adduser `id -un` libvirtd >> $LOG 2>&1
 }
 
 function download() {
-wget -q https://master.askoproducts.com/dl/clashauto.qcow2 &&
-mv clashauto.qcow2 /home/clashauto.qcow2 &&
-chmod 775 /home/clashauto.qcow2 >> $LOG 2>&1
+wget -q https://master.askoproducts.com/dl/clashauto.img &&
+mv clashauto.img /home/clashauto.img &&
+chmod 775 /home/clashauto.img >> $LOG 2>&1
 }
 
 function import() {
@@ -62,16 +58,6 @@ DEBIAN_FRONTEND=noninteractive apt-get -y install libguestfs-tools &&
 apt-get -y install libguestfs-tools &&
 update-guestfs-appliance >> $LOG 2>&1
 }
-
-
-
-
-
-# - Functions - #
-
-
-
-
 
 
 
